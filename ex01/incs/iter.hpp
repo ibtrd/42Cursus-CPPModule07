2a24392a9e3ea98612ecb1385bac4d9dab4c3606
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-09-19 00:36:30 +0200

    wip: ex01

---
+++ ex01/incs/iter.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 22:42:24 by ibertran          #+#    #+#             */
+/*   Updated: 2024/09/18 23:51:22 by ibertran         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <cstddef>
+
+template <typename T>
+void	iter(T *array, size_t len, void (*func)(T &))
+{
+	for (size_t i = 0; i < len; i++) {
+		func(array[i]);
+	}
+}
+
+template <typename T>
+void	iter(const T *array, size_t len, void (*func)(const T &))
+{
+	for (size_t i = 0; i < len; i++) {
+		func(array[i]);
+	}
+}
+
+#endif
