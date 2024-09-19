3130fcaa22a8ecaab629040f54dc5f921c9c2391
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-09-19 23:16:33 +0200

    feat: add ex01 tests

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -11,9 +11,46 @@
 /* ************************************************************************** */
 
 #include <iostream>
-#include "whatever.hpp"
+
+#include "iter.hpp"
+#include "print.hpp"
+
+void	incr(char &c) {
+	c++;
+}
+
+void	mult(int &c) {
+	c *= 5;
+}
+
+void 	print(const int &c) {
+	std::cout << c << " ";
+}
 
 int main( void ) {
+	std::cout << "Test00:\n";
+	{
+		char array[] = "012345678";
 
+		::printarray<char>(array, 9);
+		::iter<char>(array, 9, &incr);
+		::printarray<char>(array, 9);
+	}
+	std::cout << "\nTest01:\n";
+	{
+		int array[] = {2, 5, 6, 8, 1};
+
+		::printarray<int>(array, 5);
+		::iter<int>(array, 5, &mult);
+		::printarray<int>(array, 5);
+	}
+	std::cout << "\nTest02:\n";
+	{
+		const int array[] = {2, 5, 6, 8, 1};
+
+		::printarray<const int>(array, 5);
+		::iter<const int>(array, 5, &print);
+		std::cout << std::endl;
+	}
 	return 0;
 }
