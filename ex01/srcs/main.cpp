f6e5401434943363ce47c23ae410c9c1936d5290
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-09-25 17:13:33 +0200

    refactor: remove printarray function template

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,14 +6,13 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 21:59:17 by ibertran          #+#    #+#             */
-/*   Updated: 2024/09/21 21:30:24 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/09/25 17:12:05 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 #include "iter.hpp"
-#include "print.hpp"
 
 void	incr(char &c) {
 	c++;
@@ -23,7 +22,8 @@ void	mult(int &c) {
 	c *= 5;
 }
 
-void 	print(const int &c) {
+template <typename T>
+void 	print(const T &c) {
 	std::cout << c << " ";
 }
 
@@ -32,25 +32,28 @@ int main( void ) {
 	{
 		char array[] = "012345678";
 
-		printarray(array, 9);
+		iter(array, 9, &print);
+		std::cout << "\n";
 		iter(array, 9, &incr);
-		printarray(array, 9);
+		iter(array, 9, &print);
+		std::cout << "\n";
 	}
 	std::cout << "\nTest01:\n";
 	{
 		int array[] = {2, 5, 6, 8, 1};
 
-		printarray(array, 5);
+		iter(array, 5, &print);
+		std::cout << "\n";
 		iter(array, 5, &mult);
-		printarray(array, 5);
+		iter(array, 5, &print);
+		std::cout << "\n";
 	}
 	std::cout << "\nTest02:\n";
 	{
 		const int array[] = {2, 5, 6, 8, 1};
-
-		printarray(array, 5);
+		
 		iter(array, 5, &print);
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
 	return 0;
 }
